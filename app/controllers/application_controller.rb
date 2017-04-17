7938d3e245bc538907383be4d37577800a07ef08
James Anderson <james.p.anderson16@gmail.com>
2017-04-17 15:30:56 -0600

    requires current user for use

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,10 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def authorize!
+    unless current_user
+      render file: "public/404"
+    end
+  end
 end
