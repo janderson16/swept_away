db0f96c5fb45cf31f07123c0b663f670b86a25fe
James Anderson <james.p.anderson16@gmail.com>
2017-04-17 17:11:56 -0600

    added table to search results

--- app/models/route.rb
+++ app/models/route.rb
@@ -3,7 +3,8 @@ class Route
               :end_time,
               :street,
               :start_point,
-              :end_point
+              :end_point,
+              :side
 
   def initialize(route_info)
     @start_time = route_info[:starttime]
@@ -12,6 +13,18 @@ class Route
     @start_point = route_info[:from]
     @end_point = route_info[:to]
     @side = route_info[:side]
+    @week1 = route_info[:week1]
+    @week2 = route_info[:week2]
+    @week3 = route_info[:week3]
+    @week4 = route_info[:week4]
+    @week5 = route_info[:week5]
+    @mon = route_info[:monday]
+    @tues = route_info[:tuesday]
+    @weds = route_info[:wednesday]
+    @thurs = route_info[:thursday]
+    @fri = route_info[:friday]
+    @sat = route_info[:saturday]
+    @sun = route_info[:sunday]
   end
 
   # def self.first_mon
