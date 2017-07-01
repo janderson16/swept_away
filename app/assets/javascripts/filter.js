59f162f13f500f4d314778e994619ed91d41a7bd
James Anderson <james.p.anderson16@gmail.com>
2017-06-30 18:07:10 -0600

    undo precompile and filter

--- app/assets/javascripts/filter.js
+++ app/assets/javascripts/filter.js
@@ -1,13 +1,31 @@
 $(document).ready(function () {
-  $('#route_filter').keyup(function() {
-    var that = this
-    $.each($('.route-name').parent(),
-    function(i, val) {
-      if ($(val).text().toLowerCase().indexOf($(that).val()) == -1) {
-        $('.route-row').eq(i).hide()
-      } else {
-        $('.route-row').eq(i).show()
-      }
-    })
-  })
-});
+
+  var $routes = $('.route');
+
+  $('#route_filter').on('keyup', function() {
+      var currentName = this.value;
+
+      $routes.each(function (index, route){
+        $route = $(route);
+        if ($route.html().indexOf(currentName) !== -1) {
+          $route.show();
+        } else {
+          $route.hide();
+        }
+      });
+    });
+  });
+//
+// $(document).ready(function () {
+//   $('#route_filter').keyup(function() {
+//     var that = this
+//     $.each($('.route-name').parent(),
+//     function(i, val) {
+//       if ($(val).text().toLowerCase().indexOf($(that).val()) == -1) {
+//         $('.route-row').eq(i).hide()
+//       } else {
+//         $('.route-row').eq(i).show()
+//       }
+//     })
+//   })
+// });
